b246c3f0ddb10a52fe9befbd784d23bb411cd526
Kartikey Semwal <55513264+kartikeysemwal@users.noreply.github.com>
2021-12-23 00:27:47 +0530

    completed project

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -5,74 +5,74 @@ const db = require("../_helpers/db");
 const User = db.User;
 
 module.exports = {
-  login,
-  getAll,
-  getById,
-  getByUserName,
-  create,
-  update,
-  delete: _delete,
+    login,
+    getAll,
+    getById,
+    getByUserName,
+    create,
+    update,
+    delete: _delete,
 };
 
 async function login({ username, password }) {
-  const user = await User.findOne({ username });
-  if (user && bcrypt.compareSync(password, user.hash)) {
-    const token = jwt.sign({ sub: user.id }, config.secret, {
-      expiresIn: "7d",
-    });
-    return {
-      ...user.toJSON(),
-      token,
-    };
-  }
+    const user = await User.findOne({ username });
+    if (user && bcrypt.compareSync(password, user.hash)) {
+        const token = jwt.sign({ sub: user.id }, config.secret, {
+            expiresIn: "7d",
+        });
+        return {
+            ...user.toJSON(),
+            token,
+        };
+    }
 }
 
 async function getAll() {
-  return await User.find();
+    return await User.find();
 }
 
 async function getById(_id) {
-  return await User.findById({ _id });
+    return await User.findById({ _id });
 }
 async function getByUserName(username) {
-  return await User.findOne({ username });
+    return await User.findOne({ username });
 }
 
 async function create(userParam) {
-  if (await User.findOne({ username: userParam.username })) {
-    throw 'Username "' + userParam.username + '" is already taken';
-  }
+    if (await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
 
-  const user = new User(userParam);
+    const user = new User(userParam);
 
-  if (userParam.password) {
-    user.hash = bcrypt.hashSync(userParam.password, 10);
-  }
+    if (userParam.password) {
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+    }
 
-  await user.save();
-  return user;
+    await user.save();
+    return user;
 }
 
 async function update(id, userParam) {
-  const user = await User.findById(id);
+    const user = await User.findById(id);
 
-  if (!user) throw "User not found";
-  if (
-    user.username !== userParam.username &&
-    (await User.findOne({ username: userParam.username }))
-  ) {
-    throw 'Username "' + userParam.username + '" is already taken';
-  }
+    if (!user) throw "User not found";
+    if (
+        user.username !== userParam.username &&
+        (await User.findOne({ username: userParam.username }))
+    ) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
 
-  if (userParam.password) {
-    userParam.hash = bcrypt.hashSync(userParam.password, 10);
-  }
+    if (userParam.password) {
+        userParam.hash = bcrypt.hashSync(userParam.password, 10);
+    }
 
-  Object.assign(user, userParam);
+    Object.assign(user, userParam);
 
-  await user.save();
+    await user.save();
 }
 
 async function _delete(id) {
-  await User.findByIdAndRemove(id);
+    await User.findByIdAndRemove(id);
 }
